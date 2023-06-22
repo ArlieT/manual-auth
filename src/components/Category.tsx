31904e20ed8bc377a2046a8c5c096f9361c39a4e
ArlieT <90963743+arlieboyk@users.noreply.github.com>
2023-06-22 21:03:48 +0800

    fix

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -17,14 +17,15 @@ function Category({ categoryNumber }:any) {
   ];
 
   return (
-    <div className='embla__slide w-screen pl-[1rem] relative border border-red-500 mx-2 flex flex-col items-center'>
-      <div className={`embla__slide__number w-[4.6rem] h-[4.6rem] z-10 top-[0.6rem] right-[0.6rem] border-[50%] font-bold pointer-events-none bg-gradient-to-r from-${categoryData.iconColor}-500 to-${categoryData.iconColor}-700`}>
-        <span className='bg-clip-text block absolute inset-0'>{categoryNumber}</span>
-      </div>
-      <Image src={categoryData.image} alt='' />
-      <BiBed size={categoryData.iconSize} className='embla__slide__img' />
-      <p>{categoryData.title}</p>
-    </div>
+    // <div className='embla__slide w-screen pl-[1rem] relative border border-red-500 mx-2 flex flex-col items-center'>
+    //   <div className={`embla__slide__number w-[4.6rem] h-[4.6rem] z-10 top-[0.6rem] right-[0.6rem] border-[50%] font-bold pointer-events-none bg-gradient-to-r from-${categoryData.iconColor}-500 to-${categoryData.iconColor}-700`}>
+    //     <span className='bg-clip-text block absolute inset-0'>{categoryNumber}</span>
+    //   </div>
+    //   <Image src={categoryData.image} alt='' />
+    //   <BiBed size={categoryData.iconSize} className='embla__slide__img' />
+    //   <p>{categoryData.title}</p>
+    // </div>
+    <></>
   );
 }
 
