a5ccbb12ea5620da74fab70d377c54fc82ebf6a8
ArlieT <90963743+arlieboyk@users.noreply.github.com>
2023-06-22 21:15:20 +0800

    postinstall

--- src/app/api/addToCart/route.ts
+++ src/app/api/addToCart/route.ts
@@ -3,33 +3,31 @@ import { NextResponse } from "next/server"
 
 
 
+export async function POST(req: Request) {
+  const { productId, quantity, userEmail, userId } = await req.json()
+  console.log({ productId, quantity, userEmail })
+  try {
 
-
-export async function POST(req:Request){
-    const { productId, quantity,userEmail,userId }= await req.json()
-    console.log({productId , quantity ,userEmail})
-    try {
-
-      const user = await prisma.user.findFirst({
-        where:{
-          email:userEmail
-        }
-      })
-        console.log('email ',user)
-      if (!user) {
-        return NextResponse.json({ error: 'user not found' },{status:404});
+    const user = await prisma.user.findFirst({
+      where: {
+        email: userEmail
       }
+    })
+    console.log('email ', user)
+    if (!user) {
+      return NextResponse.json({ error: 'user not found' }, { status: 404 });
+    }
 
 
-        const product = await prisma.product.findFirst({
-            where:{
-                id: productId
-            }
-        })
+    const product = await prisma.product.findFirst({
+      where: {
+        id: productId
+      }
+    })
 
-        
+
     if (!product) {
-      return NextResponse.json({ error: 'Product not found' },{status:404});
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 });
     }
 
 
@@ -41,22 +39,22 @@ export async function POST(req:Request){
       },
     });
 
-    console.log('created ',cartItem);
-    if(cartItem){
+    console.log('created ', cartItem);
+    if (cartItem) {
       console.log('res ', cartItem)
-    return NextResponse.json({ cartItem });
-    }else{
+      return NextResponse.json({ cartItem });
+    } else {
 
-      return NextResponse.json({message:"error adding to cart"},{status:400})
+      return NextResponse.json({ message: "error adding to cart" }, { status: 400 })
     }
-    
-     
 
-    } catch (error) {
-      console.log({error})
-        NextResponse.json({message:error},{status:500})
-    }
-}   
+
+
+  } catch (error) {
+    console.log({ error })
+    NextResponse.json({ message: error }, { status: 500 })
+  }
+}
 
 
 
